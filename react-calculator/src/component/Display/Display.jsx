e4afdf697ff3f4c81217e84c94435634cc62c837
Jully Silva <jkasilva@sga.pucminas.br>
2023-02-25 14:10:13 -0300

    retirando o console log

--- react-calculator/src/component/Display/Display.jsx
+++ react-calculator/src/component/Display/Display.jsx
@@ -4,7 +4,6 @@ import "./Display.css";
 export const Display = ({ value }) => {
     return (
         <div className="componet-display">
-            {console.log('passei aqui')}
             <div>{value}</div>
         </div>
     );
